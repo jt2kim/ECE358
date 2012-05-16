f780cbfc46e05c8b3ffb46e46446e1769a2a3c50
Jay <jaymahendru@gmail.com>
2012-05-16 16:27:01 -0400

    added code for main();

--- Lab1/lab1.cc
+++ Lab1/lab1.cc
@@ -1,8 +1,25 @@
 #include "lab1.h"
 
 int main() {
+	int j;
+	double u,lambda=100,x,sum=0,Mean,var1=0,Variance;
 
-
+	sgenrand(4357); /* any nonzero integer can be used as a seed */
+		    	
+	for (j=0; j<1000; j++) {
+		u = genrand();
+		x=(-1/lambda)*log(1-u);
+		printf("urand = %5f \n", genrand());
+		printf("exp. rv = %5f\n ",x);
+		sum = sum +x;
+		var1=var1 + x*x;		
+		if (j%8==7) printf("\n");
+	}
+	Mean = sum/1000;
+	Variance = 0.001*var1-(Mean*Mean);
+	printf("Mean = %5f\n",Mean);
+	printf("Variance = %5f\n",Variance);
+	printf("\n");
 }
 
 void startSimulation(int ticks) {

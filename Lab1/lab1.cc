cc71c1b51dde7bd9cae8e9501cdce2857f9f53cb
Jay <jaymahendru@gmail.com>
2012-05-16 18:00:45 -0400

    Added random number generation and main code

--- Lab1/lab1.cc
+++ Lab1/lab1.cc
@@ -1,4 +1,5 @@
 #include "lab1.h"
+#include <iostream>
 
 using namespace std;
 
@@ -8,35 +9,20 @@ extern "C" {
 queue<int> buffer;
 list<int> averageInQueue;
 list<double> averageDelay;
-list<double> averageIdleTIme;
+list<double> averageIdleTime;
+double t_arrival;
+double t_departure;
+int bufferSize;
+int ticks = 100;
 
 int main() {
-	int j;
-	double u,lambda=100,x,sum=0,Mean,var1=0,Variance;
+    /* Initialization */
+    sgenrand(4357);
+    t_arrival = genrand() * 1000000;
+    t_departure = t_arrival;
 
-	sgenrand(4357); /* any nonzero integer can be used as a seed */
-		    	
-	for (j=0; j<1000; j++) {
-		u = genrand();
-		x=(-1/lambda)*log(1-u);
-		printf("urand = %5f \n", genrand());
-		printf("exp. rv = %5f\n ",x);
-		sum = sum +x;
-		var1=var1 + x*x;		
-		if (j%8==7) printf("\n");
-	}
-	Mean = sum/1000;
-	Variance = 0.001*var1-(Mean*Mean);
-	printf("Mean = %5f\n",Mean);
-	printf("Variance = %5f\n",Variance);
-	printf("\n");
-    
-    /*
-    initialization
-    
     startSimulation(ticks);
     computePerformances();
-    */
 }
 
 void startSimulation(int ticks) {
@@ -47,7 +33,10 @@ void startSimulation(int ticks) {
 }
 
 int arrival(int t) {
+	cout << t_arrival << endl;
+
 	
+		
 }
 
 int departure (int t) {

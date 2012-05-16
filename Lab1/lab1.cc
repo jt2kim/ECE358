74882283e02507eb452c75869c1ceb26a94fc440
Jay <jaymahendru@gmail.com>
2012-05-16 16:21:45 -0400

    added function skeletons

--- Lab1/lab1.cc
+++ Lab1/lab1.cc
@@ -1,1 +1,27 @@
 #include "lab1.h"
+
+int main() {
+
+
+}
+
+void startSimulation(int ticks) {
+	for (int t = 1; t <= ticks; t++) {
+		Arrival(t);
+		Departure(t);
+	}
+}
+
+void arrival(int t) {
+
+}
+
+void departure (int t) {
+
+}
+
+
+void computePerformances() {
+	
+}
+

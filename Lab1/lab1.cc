6ca94b812875c6c1dd6a14d3c07c5c024a566ff3
JTK <jt2kim@uwaterloo.ca>
2012-05-28 20:30:16 -0400

    trying to fix queue size calculation

--- Lab1/lab1.cc
+++ Lab1/lab1.cc
@@ -12,19 +12,20 @@ queue<double> buffer;
 int t_arrival;
 int t_departure;
 int t_leave;
+int transmissionTime;
 int bufferSize = -1;
-int ticks = 5000;
-int idleTime = 0;
+int ticks = 6000;
 
-long long runningQueueSizeSum = 0;
-long queueSizeCtr = 0;
+double runningQueueSizeSum = 0;
+double queueSizeCtr = 0;
 
-long long runningDelaySizeSum = 0;
-long delaySizeCtr = 0;
+double currentlyServing = -1;
+double u;
 
-long long runningIdleSizeSum = 0;
-long idleSizeCtr = 0;
-
+//GET THESE AS USER INPUTS 
+double lambda = 1000;
+double L = 2000;
+double C = 1; //Transmission rate of the output link (get it as bits per second but conver to bits per microsecond)
 int main(int argc, char* argv[]) {
 	// Get input
 	char* input = argv[1];
@@ -40,95 +41,104 @@ int main(int argc, char* argv[]) {
 	}
 
 	// Seed random number generator
-	sgenrand(4357);
-
+	sgenrand(4557);
+    u = genrand();
+    
+    transmissionTime = L / C;
+    
 	// Randomly generate arrival time
-	t_arrival = int(genrand() * 1000000);
+	t_arrival = (int)((-1/lambda)*log(1-u) * 1000000);
 	t_departure = t_arrival;
-    t_leave = t_departure;
-
+    t_leave = transmissionTime;
+    
 	startSimulation(ticks);
 	computePerformances();
 	cout << "done" <<endl;
 }
 
-void startSimulation(int ticks) {
+void startSimulation(int ticks) {    
 	// Runs 5 billion times
 	// Two numbers make it easy to store and manage number of runs
-	for (int t = 1; t < ticks; t++) {
-		for(int m = 1; m <= 1000000; m++)
-		{
+	for (double t = 1.0; t < (double)ticks*1000000; t+=1.0) {	
+        
+        // Check queue size and add up its value
+        runningQueueSizeSum += (double)(buffer.size());       
+        queueSizeCtr += 1;
+        
 			// If buffer is empty, increment idleTime
 			if (buffer.size() == 0) {
-				idleTime++;
-			}
-            t_arrival--;
-            t_leave--;
-			arrival(t,m);
-			departure(t,m);
-		}
+				//idleTime++;
+			}            
+            
+			arrival(t);
+			departure(t);
+		
 		// Each t is worth a 1000000
-		cout << t << endl;
+		//cout << t << endl;
 	}
-
 }
 
-int arrival(int t_1, int t_2) {
+void arrival(double t) {
 	// Check if we have randomly generated arrival time has passed
 	//if ( ((t_1 % t_arrival) == 0) || ((t_2 % t_arrival) == 0) ) {
+    t_arrival--;
     if(t_arrival <= 0 ){
-		if (buffer.size() == 0)
+    
+        //if buffer is empty and nothing is being served, the packet goes directly to the server
+		if ( (currentlyServing == -1)  && ( buffer.size() == 0) )
 		{
-			runningIdleSizeSum += idleTime;
-			idleSizeCtr++;
-		}            
-		// If buffer is not full, add packet to buffer
-		if ( (bufferSize == -1) || (bufferSize > buffer.size()) ) {
-			buffer.push( (double)(t_1 - 1)*1000000.00 + (double)t_2 );					
+            currentlyServing = t;
 		}
-        // Since buffer is no longer idle, set idleTime to 0
-	    idleTime = 0;	
-        // Generate a new arrival time for package
-	    t_arrival = (int)(genrand() * 1000000);
-        
+        else
+        {
+            // If buffer is not full, add packet to buffer
+            if ( (bufferSize == -1) || (bufferSize > buffer.size()) ) {
+                buffer.push( t );					
+            }
+            // Since buffer is no longer idle, set idleTime to 0
+            //idleTime = 0;	
+            // Generate a new arrival time for package
+            
+        }
+        u = genrand();
+        t_arrival =  (int)((-1/lambda)*log(1-u) * 1000000);
     }
 }
 
-int departure (int t_1, int t_2) {
+void departure (double t) {
+
 	// Store buffer size
 	int queueSize = buffer.size();
 	
 	// If buffer is empty, do nothing
-	if(queueSize == 0)
+	//if(queueSize == 0)
+	//	return 0;
+        //increment idle here
+	if(queueSize > 0 )
+    {
+        if ( ( fmod(t , (double)t_departure) <= 0) && (currentlyServing == -1) )
+        {
+            currentlyServing = buffer.front();
+            buffer.pop();
+        }
+    }
+    
+    if(currentlyServing != -1)
+    {
+        t_leave--;
+        if( t_leave <= 0 )
+        {            
+            currentlyServing = -1;
+            t_leave = transmissionTime;
+        }
+    }
 
-		return 0;
-		
-	// If buffer is not empty	
-	else
-	{
-		// Check that set departure time has passed
-		if( t_leave <=0 /*(t_1 % t_departure) == 0 || (t_2 % t_departure) == 0 */)
-		{
-			// Remove packet from queue and send to server
-			double packetBeingServiced = buffer.front();
-			buffer.pop();
-			
-			// Check queue size and add up its value
-			runningQueueSizeSum += (double)(queueSize - 1);            
-			queueSizeCtr += 1;
-
-			// Get arrival time of packetBeingServiced
-			runningDelaySizeSum +=  ( (double)(t_1 - 1)*1000000.0 + (double)t_2 ) - packetBeingServiced;
-			delaySizeCtr += 1;
-            t_leave = t_departure;
-		}        
-	}
 
 }
 
 void computePerformances() {
 cout << queueSizeCtr << "    lol     " << runningQueueSizeSum<<endl;
-	cout << "Average Size of Queue is :    " << runningQueueSizeSum/(double)queueSizeCtr << endl;
-	cout << "Average Delay is         :    " << runningDelaySizeSum/delaySizeCtr << endl;
-	cout << "Average Idle time is     :    " << runningIdleSizeSum/idleSizeCtr << endl;
+	cout << "Average Size of Queue is :    " << runningQueueSizeSum/queueSizeCtr << endl;
+	//cout << "Average Delay is         :    " << runningDelaySizeSum/delaySizeCtr << endl;
+	//cout << "Average Idle time is     :    " << runningIdleSizeSum/idleSizeCtr << endl;
 }

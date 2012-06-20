70e455f90ba97f7cf3ccb7288f51f0d4a16e27a2
JTK <jt2kim@uwaterloo.ca>
2012-06-20 14:30:01 -0400

    commit to resolve conflicts

---
+++ Lab1/Test/lab1.h
@@ -0,0 +1,12 @@
+#ifndef LAB_ONE
+#define LAB_ONE
+#include <queue>
+#include <list>
+#include <math.h>
+
+void startSimulation(int ticks);
+void arrival(double t);
+void departure(double t);
+void computePerformances();
+
+#endif

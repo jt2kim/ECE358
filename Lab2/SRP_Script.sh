e991caf554462857c9e0757a44f11bbbd37d7224
JTK <jt2kim@uwaterloo.ca>
2012-07-08 17:18:39 -0400

    fixed the script

--- Lab2/SRP_Script.sh
+++ Lab2/SRP_Script.sh
@@ -1,18 +1,16 @@
 #!/bin/bash
 Ws=( 5 15 )
-for W in ${Ws[@]} 
-do
-    echo window size $W
-    for ((i=10; i<100; i+=10))
-    do
-    Window=5
-    echo $i
-    #./simulator $i $W
-    mv layer2.txt layer2_$i.txt
-    mv layer2_$i.txt 5_2
-    mv layer3.txt layer3_$i.txt
-    mv layer3_$i.txt 5_3
 
-    done
+for ((i=10; i<100; i+=10))
+do
+Window=5
+echo $i
+./simulator $i $Window
+mv layer2.txt layer2_$i.txt
+mv layer2_$i.txt 5_2
+mv layer3.txt layer3_$i.txt
+mv layer3_$i.txt 5_3
+
 done
+
 exit 0
